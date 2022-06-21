f1e75343ffd9fd4de56bc22dbf0721b0ce1ecd7e
arareabe <abekim143@gmail.com>
2022-06-21 10:44:37 -0700

    Fifth problem

--- starter/problems/05-reverse.js
+++ starter/problems/05-reverse.js
@@ -12,7 +12,19 @@ reverse("id"); // "di"
 reverse(""); // ""
 ***********************************************************************/
 
-// your code here
+function reverse(str) {
+  if (str.length === 0) {
+    return '';
+  }
+  return reverse(str.slice(1)) + str[0];
+}
+
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
